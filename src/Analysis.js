5358f41d4f5a22aa6ad774818aff17340206c4f1
Vinoth07 <sangaralingamvinoth64@gmail.com>
2025-01-19 17:15:29 +0530

    post-sessions-added

--- src/Analysis.js
+++ src/Analysis.js
@@ -1,13 +1,18 @@
 import UserListComponent from './UserListComponent';
 import FormComponent from "./FormComponent";
+
 const Analysis = () => {
     return (
-        <div className="container mx-auto p-4">
-            <h2 className="text-2xl text-gray-800 font-bold mb-6 text-center">Analysis Page</h2>
-            <FormComponent />
-            <div className="p-6">
-                <h2 className="text-2xl font-bold text-gray-800 mb-6">User List</h2>
-                <UserListComponent />
+        <div className="container mx-auto p-6 bg-gray-100 rounded-md shadow-md">
+            <h2 className="text-2xl text-gray-200 font-bold mb-6 text-center bg-zinc-800 p-4 rounded-md">Analysis Page</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <div className="bg-white p-6 rounded-md shadow-sm">
+                    <FormComponent />
+                </div>
+          
+                <div className="bg-white p-6 rounded-md shadow-sm">
+                    <UserListComponent />
+                </div>
             </div>
         </div>
     );
