118f81736978942d38c6f434c77cf19aa285b1df
Vinoth07 <sangaralingamvinoth64@gmail.com>
2025-02-21 00:52:46 +0530

    del-nose

--- src/PoseDetector.js
+++ src/PoseDetector.js
@@ -4,7 +4,7 @@ import Webcam from "react-webcam";
 import { io } from "socket.io-client";
 import ProgressBar from "@ramonak/react-progress-bar";
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';  
+import { useNavigate } from 'react-router-dom';
 import Countdown from 'react-countdown';
 const socket = io("http://127.0.0.1:5000");  // Connect to Flask server
 
@@ -12,7 +12,7 @@ const PoseDetector = () => {
     const webcamRef = useRef(null);
     const [isCapturing, setIsCapturing] = useState(false);
     const [processedImage, setProcessedImage] = useState(null);
-    const [noseMessage, setNoseMessage] = useState("");
+
     const [shoulderMessage, setShoulderMessage] = useState("");
     const [angles, setAngles] = useState({});
     const [alignmentPercentage, setAlignmentPercentage] = useState(0);
@@ -26,7 +26,7 @@ const PoseDetector = () => {
     const [headAlignmentPercentage, setHeadAlignmentPercentage] = useState(0);
     const [alignmentPercentages, setAlignmentPercentages] = useState([]);
     const [headAlignmentPercentages, setHeadAlignmentPercentages] = useState([]);
-    const navigate = useNavigate(); 
+    const navigate = useNavigate();
 
     // Use a ref to store the latest alignment percentages
     const alignmentPercentagesRef = useRef([]);
@@ -72,24 +72,22 @@ const PoseDetector = () => {
                 startTime,
                 endTime
             );
-    
+
             const sessionData = {
-                session_name: "Posture Session new1",
+                session_name: "Posture Session",
                 user_id: userId,
                 posture_type: "Seated",
                 start_time: new Date(startTime).toISOString(),
                 end_time: new Date(endTime).toISOString(),
                 ...postureStats, // Spread the returned object into sessionData
             };
-    
+
             console.log("Session Data:", sessionData);
 
-            
+
             axios.post('http://127.0.0.1:5000/sessions', sessionData)
                 .then((response) => console.log("Session saved:", response.data))
                 .catch((error) => console.error("Error saving session:", error));
-
-            navigate('/analysis');
         }
     }, [startTime, endTime]);
 
@@ -101,28 +99,11 @@ const PoseDetector = () => {
         }
     }, [isCapturing]);
 
-    // useEffect(() => {
-    //     // Listen for processed frames from the backend
-
-    //     socket.on("processed_frame", (data) => {
-    //         setProcessedImage(`data:image/jpeg;base64,${data.image}`);
-    //         setNoseMessage(data.nose_message);
-    //         setShoulderMessage(data.shoulder_message);
-    //         setAngles(data.angles);
-    //         setAlignmentPercentage(data.overall_percentage);
-    //         setHeadHorizontalMessage(data.head_horizontal_message);
-    //         setHeadAlignmentPercentage(data.head_horizontal_percentage);
-    //         setHeadAlignmentPercentages((prev) => [...prev, data.head_horizontal_percentage]);
-            
-    //         console.log(headAlignmentPercentages);
-    //     });
-    // }, []);
 
     useEffect(() => {
         // Listen for processed frames from the backend
         socket.on("processed_frame", (data) => {
             setProcessedImage(`data:image/jpeg;base64,${data.image}`);
-            setNoseMessage(data.nose_message);
             setShoulderMessage(data.shoulder_message);
             setAngles(data.angles);
             setAlignmentPercentage(data.overall_percentage);
@@ -151,7 +132,7 @@ const PoseDetector = () => {
 
     useEffect(() => {
     }, [alignmentPercentages, headAlignmentPercentages]);
-    
+
     useEffect(() => {
         const fetchUserData = async () => {
             try {
@@ -211,14 +192,18 @@ const PoseDetector = () => {
         </div>;
     }
 
+    const goToAnalysis = () => {
+        navigate("/analysis");
+    };
+
     return (
         <div className="container mx-auto p-4">
             <h2 className="text-2xl text-gray-200 font-bold mb-6 text-center bg-zinc-800 p-4 rounded-md">Real-Time Pose Detection for user: {user.name}</h2>
 
-            <div className="grid grid-cols-3 gap-6 items-start">
+            <div className="grid grid-cols-5 gap-6 items-start">
                 {/* Webcam Section */}
-                <div className="col-span-1 flex items-center justify-start">
-                    <div className="flex flex-col items-start w-full max-w-md p-4 bg-white rounded-lg shadow-2xl">
+                <div className="col-span-2 flex items-center justify-start">
+                    <div className="flex flex-col items-start w-full  p-4 bg-white rounded-lg shadow-2xl">
                         <Webcam
                             ref={webcamRef}
                             screenshotFormat="image/jpeg"
@@ -246,72 +231,27 @@ const PoseDetector = () => {
                 </div>
 
                 {/* Messages Section */}
-                <div className="col-span-1 bg-gray-100 p-4 rounded-lg shadow-md">
-                    <h3 className="text-lg font-semibold text-gray-800 mb-4">Messages</h3>
-                    <p className="text-sm font-medium text-gray-700 mb-2">{noseMessage}</p>
-                    <p className="text-sm font-medium text-gray-700">{shoulderMessage}</p>
-                    <ProgressBar completed={alignmentPercentage} />
-                    <p className="text-sm font-medium text-gray-700">{headHorizontalMessage}</p>
-                    <ProgressBar completed={headAlignmentPercentage} />
-                </div>
-
-                {/* Angles Table Section */}
-                <div className="col-span-1 bg-gray-100 p-6 rounded-lg shadow-md">
-                    <h3 className="text-lg font-semibold text-gray-800 mb-4">Angles Table</h3>
-                    <div className="overflow-x-auto">
-                        <table className="min-w-full divide-y divide-gray-300">
-                            <thead className="bg-gray-200">
-                                <tr>
-                                    <th
-                                        scope="col"
-                                        className="px-6 py-3 text-left text-sm font-semibold text-gray-700 uppercase tracking-wider"
-                                    >
-                                        Angle
-                                    </th>
-                                    <th
-                                        scope="col"
-                                        className="px-6 py-3 text-left text-sm font-semibold text-gray-700 uppercase tracking-wider"
-                                    >
-                                        Value
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody className="bg-white divide-y divide-gray-200">
-                                {Object.entries(angles).map(([key, value]) => (
-                                    <tr key={key} className="hover:bg-gray-100">
-                                        <td className="px-6 py-4 text-sm text-gray-900">{key}</td>
-                                        <td className="px-6 py-4 text-sm text-gray-900">{value}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                <div className="col-span-3 flex flex-col gap-6 w-full bg-gray-100 p-4 rounded-lg shadow-md ">
+                    <div>
+                        <h3 className="text-lg font-semibold text-gray-800 mb-4">Messages</h3>
+                        <p className="text-sm font-medium text-gray-700">{shoulderMessage}</p>
+                        <ProgressBar completed={alignmentPercentage} />
+                        <p className="text-sm font-medium text-gray-700">{headHorizontalMessage}</p>
+                        <ProgressBar completed={headAlignmentPercentage} />
                     </div>
-                </div>
-            </div>
 
-            {/* Display the countdown */}
-            <div className="mt-6">
-                {/* Countdown will live update as long as capturing is active */}
-                {isCapturing && (
-                    <Countdown
-                        date={Date.now() + (isCapturing ? elapsedTime : 0)}
-                        intervalDelay={1000}
-                        precision={3}
-                        autoStart={false}  // Ensure it doesn't auto-start
-                        renderer={props => (
-                            <div>
-                                {props.total > 0 ? (
-                                    <>
-                                        <p>Time Elapsed: {Math.floor(props.total / 1000)} seconds</p>
-                                    </>
-                                ) : (
-                                    <p>Time's up!</p>
-                                )}
-                            </div>
-                        )}
-                    />
-                )}
-                <p>Total duration: {elapsedTime.toFixed(2)} seconds</p>
+                    {/* Display the countdown */}
+                    {!isCapturing &&
+                        <div className="mt-4 flex items-center justify-between">
+                            <p>Total duration: {elapsedTime.toFixed(2)} seconds</p>
+                            <button
+                                onClick={goToAnalysis}
+                                className="mt-2 px-4 py-2 bg-gray-900 text-white rounded hover:bg-blue-600"
+                            >
+                                Go to Analysis
+                            </button>
+                        </div>}
+                </div>
             </div>
         </div>
     );
